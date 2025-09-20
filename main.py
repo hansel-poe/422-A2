9df07f2094ba6f3327a20884dee877e283a718ea
Hansel Poe <hanselivander68@yahoo.com>
2025-09-20 12:48:47 -0700

    setup grid and reward function

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
-# This is a sample Python script.
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Create V table for the grid
+V = np.array([[0,0,0,1],
+              [0,np.nan,0,-1],
+              [0,0,0,0]])
 
+V_buffer = np.array([[0,0,0,1],
+              [0,np.nan,0,-1],
+              [0,0,0,0]])
+#reward function
+def r(i,j):
+    if i == 0 and j == 3:
+        return 1.0965
+    elif i == 1 and j == 3:
+        return -1.3182
+    elif i == 1 and j == 1:
+        return np.nan
+    else:
+        return -0.1657
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+x = np.zeros((3,4))
+print(x)
 
+#Function to update value of V grid using bellman equation
+def v(r, y):
+    for i in range(0,3):
+        for j in range(0,4):
+            x[i,j] = r(i,j)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Hansel')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
+v(r,3)
+print(x)
